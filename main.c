2ab14d0710cfaff2b9d7501dd7a392a7fb42f921
Michal <abxvxxl@gmail.com>
2025-04-07 23:42:44 +0200

    Started carving algrithm

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 
+typedef struct {
+    int vertex;
+    int *next;
+}Node;
+
+void dfs(Node *nodes, int partitionsTab[],int curPartSize,int maxPartSize)
+{
+    if(curPartSize>=maxPartSize)
+        return;
+
+    Node *node_it=nodes;
+    while(node_it)
+    {
+        if(partitionsTab[node_it->vertex]!=-1)//doesnt belong to specyfic partition
+        {
+            
+        }
+        node_it=node_it->next;
+    }
+}
 
 int  main()
 {
+    //lista sasiedztwa 
 
     return 0;
 }
