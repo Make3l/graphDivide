5c0182316e9707aa8796f04ac61e0b589a1fc077
Michal <abxvxxl@gmail.com>
2025-06-01 15:37:01 +0200

    Deleted missplaced files

--- tests.h
+++
@@ -1,8 +0,0 @@
-#ifndef TESTS_H
-#define TESTS_H
-
-int** createTestGraph(int n); //creates matrix (4x3) with 12 nodes, 3 columns and 4 rows,connected: up, down, right
-int** createIrregularTestGraph(int n); //creates irregular matrix
-int** createLargeTestGraph(int n, double connectionProbability); //creates matrix (n,n), that has some probability that random(or semi random because of choosen seed) edges are connected
-
-#endif
